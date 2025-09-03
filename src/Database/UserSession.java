d536fb654558af9ca76561339095818337eba058
Sonaliyadav1 <142139207+Sonaliyadav1@users.noreply.github.com>
2025-09-03 19:46:30 +0530

    Add files via upload

---
+++ src/Database/UserSession.java
@@ -0,0 +1,21 @@
+package Database;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class UserSession {
+    public static int userId;
+    public static void setUserId(int id) {
+        userId = id;
+    }
+
+    public static int getUserId() {
+        return userId;
+    }
+}
